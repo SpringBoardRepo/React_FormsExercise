eec5b2282482a7627b1ab78456d560b12c6eaad8
poorvarathore05 <poorvarathore05@gmail.com>
2021-10-01 18:13:51 -0700

    added css

--- src/TodoList/Todo.js
+++ src/TodoList/Todo.js
@@ -1,15 +1,15 @@
 
 import { Button } from 'reactstrap';
+import './Todo.css'
 
 function Todo({ id, todo, remove, isCompleted }) {
     const handleDelete = () => remove(id);
     return (
-        <div>
+        <div className="Todo">
             <li style={{ textDecoration: todo.isCompleted ? 'line-through' : "" }}>
                 {todo}
-                {<Button size='sm' color='success' onClick={() => isCompleted(id)}>Complete</Button>}
+                <Button className='Todo-btn' size='sm' color='success' onClick={() => isCompleted(id)}>Complete</Button>
                 <Button size='sm' color='danger' onClick={handleDelete}>Remove</Button>
-
             </li>
         </div>
     )
